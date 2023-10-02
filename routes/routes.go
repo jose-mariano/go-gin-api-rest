1774a19dfbc22a2d08e0536a2946eaaa64f26590
José Mariano <jose.mariano@climatempo.com.br>
2023-10-02 12:34:23 -0300

    Initial commit

---
+++ routes/routes.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"alura/gin-api-rest/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
+
+	r.GET("/", controllers.ExibePaginaIndex)
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Saudacao)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+
+	r.NoRoute(controllers.RotaNaoEncontrada)
+
+	r.Run()
+}
